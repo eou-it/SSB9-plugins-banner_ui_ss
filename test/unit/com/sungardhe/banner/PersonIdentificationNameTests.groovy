3238c0559845fc764ff3b96392348104e2b49287
unknown <rvenkate@.corp.sct.com>
2011-05-11 14:25:57 -0400

    Sample test file

---
+++ test/unit/com/sungardhe/banner/PersonIdentificationNameTests.groovy
@@ -0,0 +1,17 @@
+package com.sungardhe.banner
+
+import grails.test.*
+
+class PersonIdentificationNameTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
