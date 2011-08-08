0a29f35b9c633223d17c84985def905b6cbce3ce
Bob Rullo <robert.rullo@sungardhe.com>
2011-08-08 18:43:32 -0400

    Rev'd to banner-ui-ss:0.0.13

--- BannerUiSsGrailsPlugin.groovy
+++ BannerUiSsGrailsPlugin.groovy
@@ -28,7 +28,7 @@ class BannerUiSsGrailsPlugin {
     //
     //String version = "0.1-SNAPSHOT"
 
-    def version = "0.0.12"
+    def version = "0.0.13"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
