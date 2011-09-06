7f341442597a02f2c7a9247c7d08cfe5e680d14c
Pete Vilter <pvilter@sungardhe.com>
2011-09-06 15:15:30 -0400

    refactor validateDate(); bump to 0.0.26

--- web-app/js/common/common.js
+++ web-app/js/common/common.js
@@ -26,16 +26,16 @@ $.fn.bindFirst = function(name, fn) {
 };
 
 
-function validateDate( input ) {
+function validateDate( dateString ) {
     var validformat = /^\d{2}\/\d{2}\/\d{4}$/; //Basic check for format validity
-    if (!validformat.test(input.value)) {
+    if (!validformat.test(dateString)) {
          return false;
     }
     else {
         //Detailed check for valid date ranges
-        var monthfield = input.value.split("/")[0];
-        var dayfield = input.value.split("/")[1];
-        var yearfield = input.value.split("/")[2];
+        var monthfield = dateString.split("/")[0];
+        var dayfield = dateString.split("/")[1];
+        var yearfield = dateString.split("/")[2];
         var dayobj = new Date(yearfield, monthfield - 1, dayfield);
 
         if ((dayobj.getMonth() + 1 != monthfield) || (dayobj.getDate() != dayfield) || (dayobj.getFullYear() != yearfield)) {
