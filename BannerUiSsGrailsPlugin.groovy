281230b8e5a4f12a735dbd53f9a4b8ad65a0384c
Kevin J. Butler <kevin.butler@sungardhe.com>
2011-10-04 13:08:17 -0600

    release 0.0.61

--- BannerUiSsGrailsPlugin.groovy
+++ BannerUiSsGrailsPlugin.groovy
@@ -31,7 +31,7 @@ class BannerUiSsGrailsPlugin {
     //
     //String version = "0.1-SNAPSHOT"
 
-    def version = "0.0.60"
+    def version = "0.0.61"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
