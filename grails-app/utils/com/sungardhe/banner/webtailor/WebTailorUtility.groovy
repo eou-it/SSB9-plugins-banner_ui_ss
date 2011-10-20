34f555928bdb610bb5696dbf8fdf6eb65926c4a9
bihshya.gau <bih.shya.gau@sungardhe.com>
2011-10-20 10:46:07 -0400

    HFGE-496 Service configure Info Text from Web Tailor

---
+++ grails-app/utils/com/sungardhe/banner/webtailor/WebTailorUtility.groovy
@@ -0,0 +1,44 @@
+/** *****************************************************************************
+ � 2011 SunGard Higher Education.  All Rights Reserved.
+
+ CONFIDENTIAL BUSINESS INFORMATION
+
+ THIS PROGRAM IS PROPRIETARY INFORMATION OF SUNGARD HIGHER EDUCATION
+ AND IS NOT TO BE COPIED, REPRODUCED, LENT, OR DISPOSED OF,
+ NOR USED FOR ANY PURPOSE OTHER THAN THAT WHICH IT IS SPECIFICALLY PROVIDED
+ WITHOUT THE WRITTEN PERMISSION OF THE SAID COMPANY
+ ****************************************************************************** */
+package com.sungardhe.banner.webtailor
+import groovy.sql.Sql
+import org.codehaus.groovy.grails.web.context.ServletContextHolder as SCH
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes as GA
+
+/**
+ * This is a helper class that is used to help common validation and other processing for
+ * Web Tailor objects
+ *
+ */
+class WebTailorUtility {
+
+    public static String getInfoText(String name, String label, String source = '') {
+        def ctx = SCH.servletContext.getAttribute(GA.APPLICATION_CONTEXT)
+        def sessionFactory = ctx.sessionFactory
+        def session = sessionFactory.currentSession
+        def sql = new Sql(session.connection())
+        def sqlQueryString = """select twgrinfo_text text from twgrinfo
+	    					    where  twgrinfo_name = '${name}'
+	    					    and    twgrinfo_label = '${label}'
+	    						and twgrinfo_source_ind =
+	       						(select nvl( '${source}',nvl( max(twgrinfo_source_ind ),'B'))
+	        					from twgrinfo
+	        					where  twgrinfo_name = '${name}'
+	        					and    twgrinfo_label = '${label}'
+	        					and twgrinfo_source_ind='L')
+	    						order by twgrinfo_sequence"""
+	   
+		def infoText = ""
+		sql.rows(sqlQueryString).each {t -> infoText += t.text + "\n"}
+		return infoText
+    }
+
+}
--- test/integration/com/sungardhe/banner/webtailor/WebTailorUtilityIntegrationTests.groovy
+++ test/integration/com/sungardhe/banner/webtailor/WebTailorUtilityIntegrationTests.groovy
@@ -1,3 +1,39 @@
+/** *****************************************************************************
+ � 2010 SunGard Higher Education.  All Rights Reserved.
+
+ CONFIDENTIAL BUSINESS INFORMATION
+
+ THIS PROGRAM IS PROPRIETARY INFORMATION OF SUNGARD HIGHER EDUCATION
+ AND IS NOT TO BE COPIED, REPRODUCED, LENT, OR DISPOSED OF,
+ NOR USED FOR ANY PURPOSE OTHER THAN THAT WHICH IT IS SPECIFICALLY PROVIDED
+ WITHOUT THE WRITTEN PERMISSION OF THE SAID COMPANY
+ ****************************************************************************** */
+package com.sungardhe.banner.webtailor
+
+import com.sungardhe.banner.testing.BaseIntegrationTestCase
+
+/**
+ * This is a helper class that is used to access Web Tailor
+ *
+ */
+
+
+class WebTailorUtilityIntegrationTests extends BaseIntegrationTestCase {
+
+    protected void setUp() {
+        formContext = ['SFAALST']
+        super.setUp()
+    }
+
+
+    def testGetInfoText() {
+        
+        def infoText = WebTailorUtility.getInfoText('twbkwbis.P_ValLogin', 'WELCOME')
+        assertTrue infoText.contains('Welcome')
+        
+    }
+
+}
 <?xml version="1.0" encoding="UTF-8"?>
 <!--
 /** *****************************************************************************
@@ -21,7 +57,7 @@
   <modelVersion>4.0.0</modelVersion>
   <groupId>com.sungardhe</groupId>
   <artifactId>banner-ui-ss</artifactId>
-  <version>0.1.1</version>
+  <version>0.0.77</version>
   <packaging>zip</packaging>
   <description>A Grails Plugin providing core and UI framework for Banner Self Service</description>
   <distributionManagement>
