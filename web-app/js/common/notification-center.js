eou-it/SSB9-plugins-banner_ui_ss

commit 14d762f83c27470e2766045d48d950782ffbb253

HWF-1001: Prevent save successful message from disappearing immediately when moving to another course

web-app/js/common/notification-center.js
@@ -127,7 +127,12 @@ $(document).ready(function() {
                             var notification = new Notification( {message: message.message, type: message.type, model: model, attribute: message.field } );
 
                             if (message.type === "success") {
-                                notification.set( { flash: true, message: $.i18n.prop("js.notification.success"), ignoreForGroupBy: ["model"] } );
+                                notification.set( {
+                                    flash: true,
+                                    message: $.i18n.prop("js.notification.success"),
+                                    ignoreForGroupBy: ["model"],
+                                    model:null // Clear model so success message doesn't disappear immediately while moving to another data set
+                                } ); 
                             }
 
                             this.addNotification( notification );

commit 6f14d5ea52e012852aafb30eeca43df001c6065a

HWF-1001: Prevent clearNotifications from removing flash messages - allow them to persist until they time out or are explicitly removed.  This also allows the save successful highlight on the individual student, which was broken in 14d762f83c27470e2766045d48d950782ffbb253.

web-app/js/common/notification-center.js
@@ -79,6 +79,10 @@ $(document).ready(function() {
             if (model) {
                 // Remove all notifications that are bound to the model
                 var notificationsToRemove = this.filter( function( n ) {
+                    if (n.get( "flash" )) {
+                        return false; // allow flash notifications to persist until they time out or are explicitly removed
+                    }
+
                     var notificationModel = n.get( "model" );
                     if (notificationModel && notificationModel.id === model.id) {
                         if (attributes) {
@@ -130,8 +134,7 @@ $(document).ready(function() {
                                 notification.set( {
                                     flash: true,
                                     message: $.i18n.prop("js.notification.success"),
-                                    ignoreForGroupBy: ["model"],
-                                    model:null // Clear model so success message doesn't disappear immediately while moving to another data set
+                                    ignoreForGroupBy: ["model"]
                                 } ); 
                             }
 