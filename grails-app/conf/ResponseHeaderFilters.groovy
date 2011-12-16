f5e431191157de25ce880e486d246cffa199308e
Navein Austin Fernandes <Navein.Fernandes@sungard.com>
2011-12-16 12:22:43 +0530

    HWF-1009: Changes to remove expire cache

--- grails-app/conf/ResponseHeaderFilters.groovy
+++ grails-app/conf/ResponseHeaderFilters.groovy
@@ -5,7 +5,15 @@ class ResponseHeaderFilters {
                 if (request.getHeader('X-Requested-With')?.equals('XMLHttpRequest')) {
                     response.setHeader('Expires', '-1')
                     response.setHeader('Cache-Control', 'no-cache')
+                    response.addHeader('Cache-Control', 'no-store')
                 }
+            }
+        }
+        addNoCachingHeaders(uri: '/**') {
+            after = {
+                response.setHeader('Expires', '-1')
+                response.addHeader('Cache-Control', 'no-cache')
+                response.addHeader('Cache-Control', 'no-store')
             }
         }
     }
