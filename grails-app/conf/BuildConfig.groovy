2388794dc8767ddf0228b674209514b4dd5cc4a2
Anish Kumar <anish.kumar@sungardhe.com>
2012-04-12 15:53:28 -0400

    Updated BuildConfig and plugin.xml to use in-place plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,16 +13,18 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.plugin.location.'banner-core'="../banner_core.git"
+grails.plugin.location.'banner-codenarc'="../banner_codenarc.git"
+grails.plugin.location.'sghe-aurora'="../sghe_aurora.git"
+grails.plugin.location.'spring-security-cas'="../spring_security_cas.git"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/releases/"
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/snapshots/"
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/thirdparty/"
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/public/asm/"
 
         grailsPlugins()
         grailsHome()
@@ -33,10 +35,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        compile 'com.sungardhe:banner-core:1.0.17'
-        compile 'com.sungardhe:spring-security-core:1.0.1'
-        compile 'com.sungardhe:banner-codenarc:0.1.3'
-        compile 'com.sungardhe:sghe-aurora:0.0.6'
+        compile ':spring-security-core:1.0.1'
 
         compile ':cache-headers:1.1.5'
         compile ':codenarc:0.8'
@@ -47,7 +46,7 @@ grails.project.dependency.resolution = {
 
         compile ':selenium:0.6'
         compile ':selenium-rc:1.0.2'
-        compile ':spring-security-cas:1.0.2'
+
         compile ':tomcat:1.3.7'
 
         provided ':maven-publisher:0.8.1'
