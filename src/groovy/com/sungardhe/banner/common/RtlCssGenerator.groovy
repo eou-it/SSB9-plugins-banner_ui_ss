ef4d7f84b9d85d4a99430dae857d75a9a1c0f761
Anish Kumar <anish.kumar@sungardhe.com>
2012-06-01 16:51:54 +0530

    Corrected the braces

--- src/groovy/com/sungardhe/banner/common/RtlCssGenerator.groovy
+++ src/groovy/com/sungardhe/banner/common/RtlCssGenerator.groovy
@@ -1,12 +1,12 @@
 /*********************************************************************************
  Copyright 2009-2012 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of 
- SunGard Higher Education and its subsidiaries. Any use of this software is limited 
- solely to SunGard Higher Education licensees, and is further subject to the terms 
- and conditions of one or more written license agreements between SunGard Higher 
+ This copyrighted software contains confidential and proprietary information of
+ SunGard Higher Education and its subsidiaries. Any use of this software is limited
+ solely to SunGard Higher Education licensees, and is further subject to the terms
+ and conditions of one or more written license agreements between SunGard Higher
  Education and the licensee in question. SunGard is either a registered trademark or
  trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher
  Education in the U.S.A. and/or other regions and/or countries.
  **********************************************************************************/
 package com.sungardhe.banner.common
@@ -310,7 +310,7 @@ class RtlCssGenerator {
 
         if (includePluginsDir) {
             filesToGenerate.addAll( getFilesToTransformMapList( BuildSettingsHolder.settings.projectPluginsDir ) )
-            if(new File("${BuildSettingsHolder.settings.baseDir.absolutePath}/plugins/").exists()) {            
+            if(new File("${BuildSettingsHolder.settings.baseDir.absolutePath}/plugins/").exists()) {
             	filesToGenerate.addAll( getFilesToTransformMapList( new File("${BuildSettingsHolder.settings.baseDir.absolutePath}/plugins/")) )
             }
         }
@@ -325,3 +325,4 @@ class RtlCssGenerator {
             println "Generated RTL version${filesToGenerate.size() > 1 ? "s" : "" } of ${filesToGenerate.source.canonicalFile}"
         }
     }
+}
