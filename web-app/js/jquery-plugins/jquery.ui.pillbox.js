2ff7a118b29c596f3806a2579fbbb1f9fa25273c
Jonathan Miller <jonathan.miller@ellucian.com>
2012-07-11 14:27:47 -0400

    GAF-21 added selectability to pillbox

--- web-app/js/jquery-plugins/jquery.ui.pillbox.js
+++ web-app/js/jquery-plugins/jquery.ui.pillbox.js
@@ -7,6 +7,7 @@
                 disabled:  "ui-state-disabled",
                 normal:    "ui-state-default"
             },
+            editable: false,
             items: [
                 // { name: String, abbreviation: String, enabled: Boolean (Default true), highlight: Boolean (Default false) }
             ]
@@ -17,6 +18,15 @@
             ul:   "<ul></ul>",
             li:   "<li></li>"
         },
+        updateTooltipText: function () {
+            var items = _.map( $( this.element ).find ( "." + this.options.styles.highlight ), function ( it ) {
+                return $( it ).attr( "data-name" );
+            });
+
+            var tooltipText = items.length > 0 ? items.join( $.i18n.prop( "ui.pillbox.tooltip.separator" ) ) : $.i18n.prop( "default.none" );
+
+            $( this.element ).attr( "title", $.i18n.prop( "ui.pillbox.tooltip.label", [ tooltipText ] ) );
+        },
         _create: function() {
             var self    = this,
                 list    = $( this.elements.ul ),
@@ -24,6 +34,18 @@
                 el      = $( this.element );
 
             el.addClass( this.options.styles.container );
+
+            var toggleSelectedClass = function ( event ) {
+                var el = $( event.target ).is( "li" ) ? $( event.target ) : $( event.target ).closest( "li" );
+
+                if ( !el.hasClass( self.options.styles.disabled ) ) {
+                    el.toggleClass( self.options.styles.highlight );
+                    self.updateTooltipText();
+                }
+
+                event.preventDefault();
+                event.stopPropagation();
+            };
 
             _.each( this.options.items, function ( it ) {
                 var item = $( self.elements.li ).addClass( self.options.styles.normal );
@@ -37,19 +59,23 @@
                     tooltip.push( it.name );
                 }
 
+                item.attr( "data-name", it.name );
+                item.attr( "data-abbreviation", it.abbreviation );
+
                 item.append( $( self.elements.div ).text( it.abbreviation ) );
+
+                if ( self.options.editable )
+                    item.click( toggleSelectedClass );
 
                 list.append( item );
             });
 
             el.append( list );
 
-            var tooltipText = tooltip.length > 0 ? tooltip.join( $.i18n.prop( "ui.pillbox.tooltip.separator" ) ) : $.i18n.prop( "default.none" );
+            this.updateTooltipText();
 
-            el.attr( "title", $.i18n.prop( "ui.pillbox.tooltip.label", [ tooltipText ] ) );
-
-            if ( _.isFunction( self.element.tooltip ) )
-                el.tooltip();
+            // if ( _.isFunction( self.element.tooltip ) )
+            //     el.tooltip();
         }
     });
 
