e66093d1863bac0b49e7937fb404824a1ef02dd2
Kevin J. Butler <kevin.butler@sungardhe.com>
2013-02-04 12:37:52 -0700

    HFC-2217: Prevent IE from entering compatibility mode.

--- grails-app/conf/ResponseHeaderFilters.groovy
+++ grails-app/conf/ResponseHeaderFilters.groovy
@@ -1,12 +1,12 @@
 /*********************************************************************************
  Copyright 2009-2012 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of 
- SunGard Higher Education and its subsidiaries. Any use of this software is limited 
- solely to SunGard Higher Education licensees, and is further subject to the terms 
- and conditions of one or more written license agreements between SunGard Higher 
+ This copyrighted software contains confidential and proprietary information of
+ SunGard Higher Education and its subsidiaries. Any use of this software is limited
+ solely to SunGard Higher Education licensees, and is further subject to the terms
+ and conditions of one or more written license agreements between SunGard Higher
  Education and the licensee in question. SunGard is either a registered trademark or
  trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher
  Education in the U.S.A. and/or other regions and/or countries.
  **********************************************************************************/
 class ResponseHeaderFilters {
@@ -25,6 +25,7 @@ class ResponseHeaderFilters {
                 response.setHeader('Expires', '-1')
                 response.addHeader('Cache-Control', 'no-cache')
                 response.addHeader('Cache-Control', 'no-store')
+                response.setHeader( 'X-UA-Compatible', 'IE=edge' )
             }
         }
     }
