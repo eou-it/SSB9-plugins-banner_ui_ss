8807f2bf155faaa0383e8ab1a91977df82cbb730
Jonathan Miller <jonathan.miller@ellucian.com>
2013-03-22 18:16:09 -0400

    NO JIRA removed duplicate resources now provided via the i18n-core plugin directly

--- grails-app/conf/BannerSelfServiceResources.groovy
+++ grails-app/conf/BannerSelfServiceResources.groovy
@@ -110,19 +110,19 @@ modules = {
         resource url: [plugin: 'banner-ui-ss', file: 'js/select2/select2.js']
 
 
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.js']
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.plus.js']
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.picker.js']
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.picker.ext.js']
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.islamic.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.plus.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.picker.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.picker.ext.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.islamic.js']
 
-        resource url:[plugin: 'i18n-core', file: 'js/jquery.multi.calendars.picker.js']
-        resource url:[plugin: 'i18n-core', file: 'js/jquery.jeditable.multi.datepicker.js']
-        resource url:[plugin: 'i18n-core', file: 'js/jquery.multi.calendars.picker.ext.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/jquery.multi.calendars.picker.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/jquery.jeditable.multi.datepicker.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/jquery.multi.calendars.picker.ext.js']
 
-        resource url:[plugin: 'i18n-core', file: 'js/multi.calendar.init.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/multi.calendar.init.js']
 
-        resource url:[plugin: 'i18n-core', file: 'css/multiCalendar.css']
+        // resource url:[plugin: 'i18n-core', file: 'css/multiCalendar.css']
     }
 
     'bannerSelfServiceRTL' {
