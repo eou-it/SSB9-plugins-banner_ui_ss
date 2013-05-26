a4eb16c8bb093643150c08eb5befebb68a17f8cb
Jonathan Miller <jonathan.miller@ellucian.com>
2013-05-26 17:26:54 -0400

    GWEB-18 corrected war deployment, resource mismatch

--- grails-app/conf/BannerSelfServiceResources.groovy
+++ grails-app/conf/BannerSelfServiceResources.groovy
@@ -6,20 +6,63 @@ modules = {
         dependsOn "bannerSelfServiceWithoutAurora, aurora"
     }
 
-
     'jquery' {
-        dependsOn "underscore"
+        resource url:[plugin: 'banner-ui-ss', file: 'css/themeroller/jquery-ui-1.8.13-lt.gry.ov/css/custom-theme/jquery-ui-1.8.13.custom.css'], attrs:[media:'screen, projection'], disposition: 'head'
 
-        defaultBundle environment == "development" ? false : "jquery"
+        resource url:[plugin: 'banner-ui-ss', file: 'js/jquery/jquery-1.10.0.js'], disposition: 'head', nominify: true
+        // resource url:[plugin: 'banner-ui-ss', file: 'js/jquery/jquery-migrate-mute.js'], disposition: 'head', nominify: true
+        resource url:[plugin: 'banner-ui-ss', file: 'js/jquery/jquery-migrate-1.2.1.min.js'], disposition: 'head', nominify: true
+        resource url:[plugin: 'banner-ui-ss', file: 'js/jquery/jquery-ui-1.10.3.custom.js'], disposition: 'head', nominify: true
+    }
 
-        resource url:[plugin: 'banner-ui-ss', file: 'css/themeroller/jquery-ui-1.8.13-lt.gry.ov/css/custom-theme/jquery-ui-1.8.13.custom.css'], attrs:[media:'screen, projection']
-        resource url:[plugin: 'banner-ui-ss', file: 'css/jquery/jquery.ui.tooltip.css'],  attrs: [media: 'screen, projection']
+    'bootstrap' {
+        dependsOn "jquery"
 
-        resource url:[plugin: 'banner-ui-ss', file: 'js/jquery/jquery-1.10.0.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/jquery/jquery-migrate-mute.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/jquery/jquery-migrate-1.2.1.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/jquery/jquery-ui-1.10.3.custom.js']
+        defaultBundle environment == "development" ? false : "bootstrap"
 
+        resource url:[plugin: 'banner-ui-ss', file: 'bootstrap/css/bootstrap.css'],            attrs: [media: 'screen, projection']
+        resource url:[plugin: 'banner-ui-ss', file: 'bootstrap/css/bootstrap-responsive.css'], attrs: [media: 'screen, projection']
+        resource url:[plugin: 'banner-ui-ss', file: 'css/bootstrap-fixes.css'],                attrs: [media: 'screen, projection']
+
+        resource url:[plugin: 'banner-ui-ss', file: 'bootstrap/js/bootstrap.js']
+    }
+
+    'bannerSelfServiceWithoutAurora' {
+        dependsOn "jquery"
+
+        defaultBundle environment == "development" ? false : "bannerSelfService"
+
+        resource url:[plugin: 'banner-ui-ss', file: 'js/html5shim.js'],
+            disposition: 'head',
+            wrapper: { s -> "<!--[if lt IE 9]>$s<![endif]-->" }
+
+        resource url:[plugin: 'banner-ui-ss', file: 'css/banner-ui-ss.css'],             attrs:[media:'screen, projection']
+        resource url:[plugin: 'banner-ui-ss', file: 'css/notification-center.css'],      attrs:[media:'screen, projection']
+        resource url:[plugin: 'banner-ui-ss', file: 'css/jquery/jquery.ui.tooltip.css'], attrs:[media:'screen, projection']
+        resource url:[plugin: 'banner-ui-ss', file: 'css/datatables-colvis.css'],        attrs:[media:'screen, projection']
+
+        resource url:[plugin: 'banner-ui-ss', file: 'css/backbone.grid.css'], attrs:[media:'screen, projection']
+        resource url:[plugin: 'banner-ui-ss', file: 'css/backbone.pagingcontrols.css'], attrs:[media:'screen, projection']
+        resource url:[plugin: 'banner-ui-ss', file: 'css/select2/select2.css'], attrs: [media: 'screen, projection']
+
+
+        resource url:[plugin: 'banner-ui-ss', file: 'js/underscore.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/underscore.string.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/backbone.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/common/backbone-custom.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/backbone.modelbinding.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/backbone.datagridview.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/backbone.pagedcollection.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/yepnope.1.0.1-min.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/log4javascript.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/common/activity-timer.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/modernizr-2.5.3.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/ICanHaz.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/handlebars.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/common/logging.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/common/common.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/jquery-plugins/jquery.sghe.dirtycheck.js']
+        resource url:[plugin: 'banner-ui-ss', file: 'js/common/notification-center.js']
         resource url:[plugin: 'banner-ui-ss', file: 'js/jquery-plugins/jquery.hoverintent.js']
         resource url:[plugin: 'banner-ui-ss', file: 'js/jquery-plugins/jquery.jeditable.js']
         resource url:[plugin: 'banner-ui-ss', file: 'js/jquery-plugins/jquery.simplemodal-1.4.1.js']
@@ -40,71 +83,7 @@ modules = {
         resource url:[plugin: 'banner-ui-ss', file: 'js/jquery-plugins/jquery.mutate.events.js']
         resource url:[plugin: 'banner-ui-ss', file: 'js/jquery-plugins/jquery.mutate.min.js']
         resource url:[plugin: 'banner-ui-ss', file: 'js/jquery-plugins/jquery.simulate.js']
-
-        resource url:[plugin: 'banner-ui-ss', file: 'js/select2/select2.js']
-        
         resource url:[plugin: 'banner-ui-ss', file: 'js/common/ajax-manager.js']
-    }
-
-
-    'bootstrap' {
-        dependsOn "jquery"
-
-        defaultBundle environment == "development" ? false : "bootstrap"
-
-        resource url:[plugin: 'banner-ui-ss', file: 'bootstrap/css/bootstrap.css'],            attrs: [media: 'screen, projection']
-        resource url:[plugin: 'banner-ui-ss', file: 'bootstrap/css/bootstrap-responsive.css'], attrs: [media: 'screen, projection']
-        resource url:[plugin: 'banner-ui-ss', file: 'css/bootstrap-fixes.css'],                attrs: [media: 'screen, projection']
-
-        resource url:[plugin: 'banner-ui-ss', file: 'bootstrap/js/bootstrap.js']
-    }
-
-
-    'underscore' {
-        defaultBundle environment == "development" ? false : "underscore"
-
-        resource url:[plugin: 'banner-ui-ss', file: 'js/underscore.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/underscore.string.js']
-    }
-
-
-    'bannerSelfServiceWithoutAurora' {
-        dependsOn "underscore, jquery"
-
-        defaultBundle environment == "development" ? false : "bannerSelfService"
-
-        resource url:[plugin: 'banner-ui-ss', file: 'js/html5shim.js'],
-            disposition: 'head',
-            wrapper: { s -> "<!--[if lt IE 9]>$s<![endif]-->" }
-
-        resource url:[plugin: 'banner-ui-ss', file: 'css/banner-ui-ss.css'],             attrs:[media:'screen, projection']
-
-        resource url:[plugin: 'banner-ui-ss', file: 'css/notification-center.css'],      attrs:[media:'screen, projection']
-
-        resource url:[plugin: 'banner-ui-ss', file: 'css/datatables-colvis.css'],        attrs:[media:'screen, projection']
-
-        resource url:[plugin: 'banner-ui-ss', file: 'css/backbone.grid.css'], attrs:[media:'screen, projection']
-        resource url:[plugin: 'banner-ui-ss', file: 'css/backbone.pagingcontrols.css'], attrs:[media:'screen, projection']
-        resource url:[plugin: 'banner-ui-ss', file: 'css/select2/select2.css'], attrs: [media: 'screen, projection']
-
-        resource url:[plugin: 'banner-ui-ss', file: 'js/backbone.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/common/backbone-custom.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/backbone.modelbinding.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/backbone.datagridview.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/backbone.pagedcollection.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/yepnope.1.0.1-min.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/log4javascript.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/common/activity-timer.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/modernizr-2.5.3.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/ICanHaz.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/handlebars.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/common/logging.js']
-        resource url:[plugin: 'banner-ui-ss', file: 'js/common/common.js']
-
-        resource url:[plugin: 'banner-ui-ss', file: 'js/common/notification-center.js']
-
-        resource url:[plugin: 'banner-ui-ss', file: 'js/jquery-plugins/jquery.sghe.dirtycheck.js']
-
         resource url:[plugin: 'banner-ui-ss', file: 'js/datatables/jquery.dataTables.js']
         resource url:[plugin: 'banner-ui-ss', file: 'js/datatables/fnReloadAjaxPlugin.js']
         resource url:[plugin: 'banner-ui-ss', file: 'js/datatables/fnFilterClearPlugin.js']
@@ -122,20 +101,22 @@ modules = {
         resource url:[plugin: 'banner-ui-ss', file: 'js/detectmobilebrowser.js']
         resource url:[plugin: 'banner-ui-ss', file: 'js/detecttabletbrowser.js']
 
+        resource url: [plugin: 'banner-ui-ss', file: 'js/select2/select2.js']
 
-        resource url:[plugin: 'i18n-core', file: 'css/multiCalendar.css']
-  
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.js']
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.plus.js']
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.picker.js']
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.picker.ext.js']
-        resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.islamic.js']
-  
-        resource url:[plugin: 'i18n-core', file: 'js/jquery.multi.calendars.picker.js']
-        resource url:[plugin: 'i18n-core', file: 'js/jquery.jeditable.multi.datepicker.js']
-        resource url:[plugin: 'i18n-core', file: 'js/jquery.multi.calendars.picker.ext.js']
-  
-        resource url:[plugin: 'i18n-core', file: 'js/multi.calendar.init.js']
+
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.plus.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.picker.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.picker.ext.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/calendars/jquery.calendars.islamic.js']
+
+        // resource url:[plugin: 'i18n-core', file: 'js/jquery.multi.calendars.picker.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/jquery.jeditable.multi.datepicker.js']
+        // resource url:[plugin: 'i18n-core', file: 'js/jquery.multi.calendars.picker.ext.js']
+
+        // resource url:[plugin: 'i18n-core', file: 'js/multi.calendar.init.js']
+
+        // resource url:[plugin: 'i18n-core', file: 'css/multiCalendar.css']
     }
 
     'bannerSelfServiceRTL' {
