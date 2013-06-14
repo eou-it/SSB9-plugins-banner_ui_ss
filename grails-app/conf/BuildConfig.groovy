c80c13746957fe3e4ae6fa80f00d769590b8aa6a
Kevin J. Butler <kevin.butler@ellucian.com>
2013-06-14 14:14:24 -0600

    Added link to mavenCentral repository and support for PROXY_SERVER_NAME environment variable

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,12 +27,17 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
 
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
-
-        mavenRepo "http://repository.jboss.org/maven2/"
-        mavenRepo "http://repository.codehaus.org"
+        if (System.properties['PROXY_SERVER_NAME']) {
+            mavenRepo "${System.properties['PROXY_SERVER_NAME']}"
+        } else {
+            grailsPlugins()
+            grailsHome()
+            grailsCentral()
+            
+            mavenCentral()
+            mavenRepo "http://repository.jboss.org/maven2/"
+            mavenRepo "http://repository.codehaus.org"
+        }
     }
 
     plugins {
