430d844bdc720052b83c3ff67444e207abcc9cb5
Naveen Prapulla S <naveenprapulla.s@ellucian.com>
2013-07-30 19:34:01 +0530

    Changes for 9.9

---
+++ grails-app/conf/BannerSelfServiceFilterFilters.groovy
@@ -0,0 +1,58 @@
+import net.hedtech.banner.loginworkflow.PostLoginWorkflow
+import net.hedtech.banner.mep.MultiEntityProcessingService
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+import org.springframework.context.ApplicationContext
+
+import net.hedtech.banner.loginworkflow.UserAgreementFlow
+
+class BannerSelfServiceFilterFilters {
+    def springSecurityService
+    UserAgreementFlow userAgreementFlow
+    List listOfFlows = []
+
+    def filters = {
+        all(controller:  "selfServiceMenu|login|logout|survey", invert: true) {
+            before = {
+                boolean allDone = request.getSession().getAttribute("ALL_DONE")
+                 if(listOfFlows.empty){
+                     getListOfFlows()
+                 }
+                String path = request.request.strippedServletPath
+                if(springSecurityService.isLoggedIn() && !allDone && !checkIgnoreUri(path)) {
+                    if(path != null) {
+                        println "request.request.strippedServletPath " + path
+                        request.getSession().setAttribute("URI_ACCESSED", path)
+                        for(int i = 0; i < listOfFlows.size(); i++) {
+                            if(listOfFlows[i].showPage(request)) {
+                                redirect uri: listOfFlows[i].getControllerUri();
+                                return false;
+
+                            }
+                       }
+                       request.getSession().setAttribute("ALL_DONE",true)
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean checkIgnoreUri(String path) {
+        boolean isIgnoredUri = false;
+
+        for (int i = 0 ; i < listOfFlows.size(); i++) {
+            if(path?.contains(listOfFlows[i].getControllerUri())) {
+                isIgnoredUri = true
+                break;
+            }
+        }
+        return isIgnoredUri
+    }
+
+    public List getListOfFlows()
+    {
+        ApplicationContext ctx = (ApplicationContext) ApplicationHolder.getApplication().getMainContext()
+        userAgreementFlow = (UserAgreementFlow) ctx.getBean("userAgreementFlow")
+        listOfFlows = [userAgreementFlow]
+        return listOfFlows
+    }
+}
