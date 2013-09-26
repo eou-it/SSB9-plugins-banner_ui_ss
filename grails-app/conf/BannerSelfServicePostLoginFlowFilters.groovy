594d7b867dcd4be13c38b19192320856eab67270
Suganthi <Suganthi.manoharan@ellucian.com>
2013-09-26 12:18:39 +0530

    (HRU-4698) Code review comments incorporated

--- grails-app/conf/BannerSelfServicePostLoginFlowFilters.groovy
+++ grails-app/conf/BannerSelfServicePostLoginFlowFilters.groovy
@@ -1,48 +1,60 @@
 import net.hedtech.banner.security.FormContext
 import org.apache.log4j.Logger
-import org.codehaus.groovy.grails.commons.ApplicationHolder
-import org.springframework.context.ApplicationContext
 import net.hedtech.banner.loginworkflow.PostLoginWorkflow
+import org.codehaus.groovy.grails.web.servlet.GrailsUrlPathHelper
+import javax.servlet.http.HttpSession
+
 /*******************************************************************************
  Copyright 2009-2012 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 class BannerSelfServicePostLoginFlowFilters {
+    private static final String SSB_BASE_URL = "ssb"
+    private static final String SLASH = "/"
+    private static final String QUESTION_MARK = "?"
     def springSecurityService
-    static List<PostLoginWorkflow> listOfFlows = []
+    List<PostLoginWorkflow> listOfFlows = []
     private final log = Logger.getLogger(getClass())
-    static Map uriMap
-    public static final String LAST_VISITED_INDEX =  "LAST_VISITED_INDEX"
+    public static final String LAST_FLOW_COMPLETED =  "LAST_FLOW_COMPLETED"
     def filters = {
         all(controller:  "selfServiceMenu|login|logout|error|dateConverter", invert: true) {
             before = {
-
-                boolean allDone = request.getSession().getAttribute(PostLoginWorkflow.ALL_DONE)
-
+                HttpSession session = request.getSession()
+                boolean isflowCompleted = session.getAttribute(PostLoginWorkflow.FLOW_COMPLETE)
+                listOfFlows = PostLoginWorkflow.getListOfFlows()
                 log.debug "Initializing workflow classes"
-                initializeListOfFlows()
-                String path = getRequestPath(request)
-                def lastVisitedIndex = request.getSession().getAttribute(LAST_VISITED_INDEX)
-                if(springSecurityService.isLoggedIn() && path != null && !allDone && !checkIgnoreUri(path)) {
-                        request.getSession().setAttribute(PostLoginWorkflow.URI_ACCESSED, path)
-                        setFormContext()
-                        for(int i = 0; i < listOfFlows.size(); i++) {
-                            request.getSession().setAttribute(LAST_VISITED_INDEX, i)
-                            if(listOfFlows[i].showPage(request)) {
-                                log.debug "Workflow URI " + listOfFlows[i].getControllerUri()
-                                redirect uri: listOfFlows[i].getControllerUri()
-                                return false;
+                Map<String,Integer> uriMap = initializeUriMap(listOfFlows)
+                String path = getServletPath(request)
+                def lastFlowCompleted = session.getAttribute(LAST_FLOW_COMPLETED)
+                if(verifyFlowCompleted(lastFlowCompleted, path, isflowCompleted, uriMap)) {
+                    session.setAttribute(PostLoginWorkflow.URI_ACCESSED, path)
+                    setFormContext()
+                    int noOfFlows = listOfFlows.size()
+                    for(int i = 0; i < noOfFlows; i++) {
+                        session.setAttribute(LAST_FLOW_COMPLETED, i)
+                        if(listOfFlows[i].isShowPage(request)) {
+                            log.debug "Workflow URI " + listOfFlows[i].getControllerUri()
+                            redirect uri: listOfFlows[i].getControllerUri()
+                            return false;
 
-                            }
-                       }
-                       request.getSession().setAttribute(PostLoginWorkflow.ALL_DONE,true)
-                }else if (null != lastVisitedIndex && checkDisplayPage(request,lastVisitedIndex)){
-                    if (listOfFlows[lastVisitedIndex].showPage(request)){
-                        redirect uri: listOfFlows[lastVisitedIndex].getControllerUri()
+                        }
+                   }
+                   session.setAttribute(PostLoginWorkflow.FLOW_COMPLETE,true)
+                }else if (null != lastFlowCompleted && checkDisplayPage(path,lastFlowCompleted,uriMap)){
+                    if (listOfFlows[lastFlowCompleted].isShowPage(request)){
+                        redirect uri: listOfFlows[lastFlowCompleted].getControllerUri()
                         return false
                     }
                 }
             }
         }
+    }
+
+    private boolean verifyFlowCompleted(def lastFlowCompleted, String path, boolean isflowCompleted, HashMap<String, Integer> uriMap) {
+        boolean checkFlow = false
+        if (springSecurityService.isLoggedIn() && path != null ){
+            checkFlow = lastFlowCompleted==null || (!isflowCompleted && !isFlowControllerURI(path, uriMap))
+        }
+        return checkFlow
     }
 
     private setFormContext() {
@@ -51,69 +63,58 @@ class BannerSelfServicePostLoginFlowFilters {
         FormContext.set( associatedFormsList )
     }
 
-    public boolean checkIgnoreUri(String path) {
+    public boolean isFlowControllerURI(String path,Map uriMap) {
         boolean isIgnoredUri = false;
-
-        for (int i = 0 ; i < listOfFlows.size(); i++) {
-            if(path?.contains(listOfFlows[i].getControllerName())) {
-                isIgnoredUri = true
-                break;
-            }
+        String controllerName = getControllerNameFromPath(path)
+        if(uriMap.get(controllerName)!=null)
+        {
+            isIgnoredUri = true
         }
         return isIgnoredUri
     }
 
 
-    private List initializeListOfFlows() {
-        if(listOfFlows.isEmpty()){
-            ApplicationContext ctx = (ApplicationContext) ApplicationHolder.getApplication().getMainContext()
-            List<String> flows = PostLoginWorkflow.getListOfFlows();
-            int noOfFlows = flows.size()
-            uriMap = new HashMap()
-            for (int i = 0 ; i < noOfFlows; i++){
-                PostLoginWorkflow flowBean = ctx.getBean(flows[i]);
-                listOfFlows.add(flowBean)
-                uriMap.put(flowBean.getControllerName(),i);
-            }
-            return listOfFlows
+    private HashMap<String,Integer> initializeUriMap(List<PostLoginWorkflow> listOfFlows) {
+        HashMap<String,Integer> uriMap = new HashMap()
+        int noOfFlows = listOfFlows.size()
+        for (int i = 0 ; i < noOfFlows; i++) {
+            uriMap.put(listOfFlows[i].getControllerName(),i);
         }
-    }
-
-    private String getRequestPath(request) {
-        String url = request?.requestURL?.toString()
-        if(url?.contains("grails")){
-            url = getStrippedPath(url)
-            url = "/ssb" + url
-            if (request?.getQueryString()) {
-                url = url + "?" + request?.getQueryString()
-            }
-        }else{
-            url = null
-        }
-        return url
+        return uriMap
     }
 
     private String getControllerNameFromPath(String url){
-        url = getStrippedPath(url)
-        url =  url.substring(1)
-        if (url.contains("/")){
-            url = url.substring(0,url.indexOf("/"))
+        if (url.contains(SSB_BASE_URL)){
+            url = url.substring(url.indexOf(SSB_BASE_URL)+SSB_BASE_URL.length()+1);
+            if (url.contains(SLASH)){
+                url = url.substring(0,url.indexOf(SLASH))
+            }
         }
         return url
     }
 
-    private String getStrippedPath(String url) {
-        url = url.substring(url.indexOf("grails/") + 6, url.indexOf(".dispatch"))
-        return url
-    }
-
-    private boolean checkDisplayPage(def request,def lastVisitedIndex ) {
-        String url = request?.requestURL?.toString()
-        if(url?.contains("grails") && null != lastVisitedIndex){
-            String controllerName = getControllerNameFromPath(url)
-            return springSecurityService.isLoggedIn() && uriMap.containsKey(controllerName) && uriMap.get(controllerName) != 0 && lastVisitedIndex != uriMap.get(controllerName)
+    private boolean checkDisplayPage(String path,def lastFlowCompleted,Map uriMap) {
+        if(path!=null && null != lastFlowCompleted){
+            String controllerName = getControllerNameFromPath(path)
+            return springSecurityService.isLoggedIn() && uriMap.containsKey(controllerName) && uriMap.get(controllerName) != 0 && lastFlowCompleted != uriMap.get(controllerName)
         }else{
             return false
         }
+    }
+
+    private getServletPath(request) {
+        GrailsUrlPathHelper urlPathHelper = new GrailsUrlPathHelper();
+        String path = urlPathHelper.getOriginatingRequestUri(request);
+        if (path!=null){
+            path = path.substring(request.getContextPath().length())
+            if (SLASH.equals(path))
+            {
+                path = null
+            }
+            else if (request?.getQueryString()) {
+                path = path + QUESTION_MARK + request?.getQueryString()
+            }
+        }
+        return path
     }
 }
