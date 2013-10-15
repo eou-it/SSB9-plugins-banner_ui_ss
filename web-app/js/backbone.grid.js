f0e76105b442e4a7bdf2508c4a98d02b7221d24f
Kevin J. Butler <kevin.butler@ellucian.com>
2013-10-15 12:43:03 -0600

    HBLR-488 hide column visibility menu if no data in table.

--- web-app/js/backbone.grid.js
+++ web-app/js/backbone.grid.js
@@ -742,6 +742,7 @@ var data = {
     toggleTableChrome: function( visible ) {
       this.table.find('thead').toggle( visible );
       this.$el.find( "." + this.css.bottom ).toggle( visible );
+      this.columnVisibilityControls.$el.toggle( visible );
 
       if ( visible ) {
         this.generatePagingControls();
