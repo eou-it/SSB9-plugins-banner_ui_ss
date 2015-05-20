c5acb5f7ee66344f00f356dc12236073301ccce8
SridhanyaG <sridhanya.ganapathi@ellucian.com>
2015-05-20 08:24:46 +0530

    Fix for HRU-5875 fix for keyboard navigation, HRU-5905 partial fix for accessibility

--- web-app/js/common/common.js
+++ web-app/js/common/common.js
@@ -152,11 +152,11 @@ window.InactivityTimer = ActivityTimer.extend({
         // If it is when the timer fires, we are goign to automatically call the logoutAction.
 
         window.setTimeout( _.bind( function() {
-            if (notifications.find( function( notification ) { return notification === n; })) {
-                this.logoutAction();
-            }
-        }, this ),
-            60 * 1000 /** 60 seconds */ );
+                if (notifications.find( function( notification ) { return notification === n; })) {
+                    this.logoutAction();
+                }
+            }, this ),
+                60 * 1000 /** 60 seconds */ );
 
     },
     activityEvents: [ "ajaxStart" ],
@@ -282,9 +282,9 @@ function showLoadingPopup( target ) {
 var hideLoadingPopup = hideLoading;
 
 /* Usage:
-     $(selector).loading();   // show loading indicator
-     $(selector).loading(false); // hide loading indicator
-*/
+ $(selector).loading();   // show loading indicator
+ $(selector).loading(false); // hide loading indicator
+ */
 $.fn.loading = function(isLoading) {
     (isLoading||isLoading==undefined) ? showLoading( this ) : hideLoading( this );
     return this;
@@ -292,7 +292,7 @@ $.fn.loading = function(isLoading) {
 
 /**
  * @deprecated use $.fn.loading
-*/
+ */
 $.fn.loadingPopup = $.fn.loading;
 
 
@@ -386,24 +386,24 @@ $(document).ready(function() {
             langDir: $.i18n.prop( "default.language.direction" ),
             resourceMap : {
                 skip_link_text :                        $.i18n.prop( "aurora.skip_link_text" ),
-                areas_label_browse :                    $.i18n.prop( "aurora.areas_label_browse" ),
-                areas_label_tools :                     $.i18n.prop( "aurora.areas_label_tools" ),
-                areas_label_browse_shortcut :           $.i18n.prop( "aurora.areas_label_browse_shortcut" ),
                 areas_label_browse_description :        $.i18n.prop( "aurora.areas_label_browse_description" ),
                 areas_label_browse_title :              $.i18n.prop( "aurora.areas_label_browse_title" ),
                 areas_label_home_title :                $.i18n.prop( "aurora.areas_label_home_title" ),
                 areas_label_home_description :          $.i18n.prop( "aurora.areas_label_home_description" ),
-                areas_label_home_shortcut :             $.i18n.prop( "aurora.areas_label_home_shortcut" ),
                 areas_label_branding:                   $.i18n.prop( "aurora.areas_label_branding" ),
-                areas_label_tools_shortcut :            $.i18n.prop( "aurora.areas_label_tools_shortcut" ),
-                areas_label_avatar_shortcut :           $.i18n.prop( "aurora.areas_label_avatar_shortcut" ),
+                areas_label_tools_title :               $.i18n.prop( "aurora.areas_label_tools_title" ),
+                areas_label_tools_description :         $.i18n.prop( "aurora.areas_label_tools_description" ),
                 openitems_label_closeSelected :         $.i18n.prop( "aurora.openitems_label_closeSelected" ),
                 openitems_label_closeAll :              $.i18n.prop( "aurora.openitems_label_closeAll" ),
                 preferences_label :                     $.i18n.prop( "aurora.preferences_label" ),
                 userdetails_signin :                    $.i18n.prop( "aurora.userdetails_signin" ),
                 userdetails_signout :                   $.i18n.prop( "aurora.userdetails_signout" ),
-                userdetails_signout_shortCut :          $.i18n.prop( "aurora.userdetails_signout_shortCut" ),
-                userdetails_signout_description :       $.i18n.prop( "aurora.userdetails_signout_description" ),
+                userdetails_signin_title :              $.i18n.prop( "aurora.userdetails_signin_title" ),
+                userdetails_signin_description :        $.i18n.prop( "aurora.userdetails_signin_description" ),
+                userdetails_guestsignin_title :         $.i18n.prop( "aurora.userdetails_guestsignin_title" ),
+                userdetails_guestsignin_description :   $.i18n.prop( "aurora.userdetails_guestsignin_description" ),
+                userdetails_profile_title :             $.i18n.prop( "aurora.areas_label_profile_title" ),
+                userdetails_profile_description :       $.i18n.prop( "aurora.areas_label_profile_description" ),
                 guestuserdetails_signin :               $.i18n.prop( "aurora.guestuserdetails_signin" ),
                 userdetails_help :                      $.i18n.prop( "aurora.userdetails_help" )
             },
@@ -467,7 +467,7 @@ key = (function(key) {
         CTRL: 4
         //META: 8 //windows key or command key (mac)
         //MOD: platform-specific, Ctrl on windows/unix or Command on Mac.  See 'mousetrap' library
-        };
+    };
     key.modifierStrings = {
         'shift':key.modifierCode.SHIFT,
         'alt':key.modifierCode.ALT,
@@ -632,16 +632,16 @@ var isMobile = function(){
 var isTablet = function(){
     var isTablet = window.matchMedia("only screen and (min-width: 769px) and (max-width:1024px)");
     if (isTablet.matches) {
-           return true;
-       }
+        return true;
+    }
     return false;
 }
 
 var isDesktop = function(){
     var isDesktop = window.matchMedia("only screen and (min-width:1025px)");
-      if (isDesktop.matches) {
-             return true;
-         }
+    if (isDesktop.matches) {
+        return true;
+    }
     return false;
 }
 
