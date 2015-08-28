4c32357f994e22d0e4f1f08707493014c57ce96f
Harshal Bramhankar <Harshal.Bramhankar@ellucian.com>
2015-08-28 20:54:00 +0530

    HRU-6003 - Fixing CtrA+CTRLV and DEL key

--- web-app/js/angular-components/locale-numeric-input/directives/only-number.js
+++ web-app/js/angular-components/locale-numeric-input/directives/only-number.js
@@ -1,9 +1,15 @@
 module.directive('onlyNumber', function() {
     return function(scope, element, attrs) {
 
-        var keyCode = [8,9,37,39,48,49,50,51,52,53,54,55,56,57,96,97,98,99,100,101,102,103,104,105,109,110,189];
+        var keyCode = [8,9,37,39,48,49,50,51,52,53,54,55,56,57,96,97,98,99,100,101,102,103,104,105,109,189,110,46];
         element.bind("keydown", function(event) {
-           if($.inArray(event.which,keyCode) == -1) {
+            if( event.ctrlKey){
+                console.log("in");
+                if(event.keyCode=="65" || event.keyCode=="86" || event.keyCode=="67"){
+                    return;
+                }
+            }else if($.inArray(event.which,keyCode) == -1) {
+
                 scope.$apply(function(){
                     scope.$eval(attrs.onlyNum);
                     event.preventDefault();
@@ -11,6 +17,6 @@ module.directive('onlyNumber', function() {
                 event.preventDefault();
             }
 
-        });
+        })
     };
 })
