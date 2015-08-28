0096550a94d8a4115bae90964a9e0c7979fcff2f
karthick <karthic.kumar@ellucian.com>
2015-08-28 18:46:55 +0530

    HRU-6003 checking in the readonly class.

--- web-app/js/angular-components/locale-numeric-input/services/readonly-service.js
+++ web-app/js/angular-components/locale-numeric-input/services/readonly-service.js
@@ -1,18 +1,63 @@
 module.service('readonlysvc', function() {
 
-	this.toggle = function(ele,value) {
+    this.toggle = function(ele,value) {
 
-		console.log('in service value is '+value);
-		console.log(value === 'true');
-		var numberInput = $(ele).find('input[type=number]');
-		if(value === 'true'){
-			numberInput.attr('readonly','readonly');
-			console.log('readonly added');
-		}
-		else{
-			numberInput.removeAttr('readonly');
-			console.log('readonly removed');
-		}
-	}
+        var numberInput = $(ele).find('input[type=number]');
+        var textInput = $(ele).find('input[type!=number]');
+        if(value === 'true'){
+            numberInput.attr('readonly','readonly');
+            $(numberInput).addClass('readonly');
+            $(textInput).addClass('readonly');
+            $(numberInput).on('click');
+        }
+        else{
+            numberInput.removeAttr('readonly');
+            $(numberInput).removeClass('readonly');
+            $(textInput).removeClass('readonly');
+            $(numberInput).off('click');
 
+        }
+
+    }
+    this.moveCursor = function(ele){
+        var numberInput = $(ele).find('input[type=number]');
+        function getSelectedText(){
+            var selText = "";
+            if (window.getSelection) {  // all browsers, except IE before version 9
+                if (document.activeElement &&
+                    (document.activeElement.tagName.toLowerCase() == "input"))
+                {
+                    var text = document.activeElement.value;
+                    selText = text.substring (document.activeElement.selectionStart,
+                        document.activeElement.selectionEnd);
+                }
+                else {
+                    var selRange = window.getSelection ();
+                    selText = selRange.toString ();
+                }
+            }
+            else {
+                if (document.selection.createRange) {       // Internet Explorer
+                    var range = document.selection.createRange ();
+                    selText = range.text;
+                }
+            }
+
+            return selText;
+        }
+        $(numberInput).on('click focus',function(){
+            var cursorPointer = this.selectionStart;
+            var len = $(this).val().length;
+            var selectedText = getSelectedText();
+            if (this.setSelectionRange){
+                if((selectedText.length === 0 && $(this).attr('readonly')) || cursorPointer === 0 ){
+                    this.setSelectionRange(len, len);
+                }
+            }
+            else{
+                $(this).val($(this).val());
+            }
+
+        });
+    }
 });
