4a02f63ca8ae368914cdc55b1fd38f9e3a957cef
karthick <karthic.kumar@ellucian.com>
2015-08-28 20:19:42 +0530

    HRU-6003 - Bug fix

--- web-app/js/angular-components/locale-numeric-input/directives/only-number.js
+++ web-app/js/angular-components/locale-numeric-input/directives/only-number.js
@@ -1,7 +1,7 @@
 module.directive('onlyNumber', function() {
     return function(scope, element, attrs) {
 
-        var keyCode = [8,9,37,39,48,49,50,51,52,53,54,55,56,57,96,97,98,99,100,101,102,103,104,105,110];
+        var keyCode = [8,9,37,39,48,49,50,51,52,53,54,55,56,57,96,97,98,99,100,101,102,103,104,105,109,110,189];
         element.bind("keydown", function(event) {
            if($.inArray(event.which,keyCode) == -1) {
                 scope.$apply(function(){
