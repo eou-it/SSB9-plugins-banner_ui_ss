64a679d92155afd0165e9e8d08b150fb329b81c3
Kevin J. Butler <kevin.butler@ellucian.com>
2016-05-03 17:44:29 -0600

    HRU-6544 Add copyright notice

--- test/unit/net/hedtech/banner/common/RtlCssGeneratorTests.groovy
+++ test/unit/net/hedtech/banner/common/RtlCssGeneratorTests.groovy
@@ -1,3 +1,6 @@
+/** *****************************************************************************
+ Copyright 2016 Ellucian Company L.P. and its affiliates.
+ ****************************************************************************** */
 package net.hedtech.banner.common
 
 import grails.test.GrailsUnitTestCase
