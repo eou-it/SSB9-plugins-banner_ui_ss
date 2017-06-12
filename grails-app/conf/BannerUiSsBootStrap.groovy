a535be24d4b4a56994fe99cfe4f525058e0d5832
Eshaan_kumar <eshaan.kumar@ellucian.com>
2017-06-12 17:37:37 +0530

    fix for HRU-7264 removing servlet context code and taking from config directly

--- grails-app/conf/BannerUiSsBootStrap.groovy
+++ grails-app/conf/BannerUiSsBootStrap.groovy
@@ -2,40 +2,18 @@
 /*******************************************************************************
 Copyright 2009-2017 Ellucian Company L.P. and its affiliates.
 *******************************************************************************/
-
-import net.hedtech.banner.controllers.ControllerUtils
-
 class BannerUiSsBootStrap {
     public static String localLogoutEnable="saml/logout?local=true";
     public static String globalLogoutEnable="saml/logout";
+    def configPropertiesService
     def grailsApplication
     def themeService
 
-	def init = { servletContext ->
-
-            def timeoutSeconds = ( grailsApplication.config.banner?.transactionTimeout instanceof Integer
-                                   ? grailsApplication.config.banner?.transactionTimeout
-                                   : 30 )
-            servletContext.setAttribute( "transactionTimeout", timeoutSeconds )
-
-        servletContext.setAttribute( "loginEndpoint", grailsApplication.config?.loginEndpoint?: "" )
-        if(ControllerUtils.isSamlEnabled()) {
-            if(ControllerUtils.isLocalLogoutEnabled()){
-                servletContext.setAttribute( "logoutEndpoint", localLogoutEnable )
-            }else{
-                servletContext.setAttribute( "logoutEndpoint", globalLogoutEnable )
-            }
-        } else {
-            servletContext.setAttribute( "logoutEndpoint", grailsApplication.config?.logoutEndpoint?: "" )
-        }
-
-        if ((true == grailsApplication.config?.guestAuthenticationEnabled) && (!"default".equalsIgnoreCase(grailsApplication.config?.banner?.sso?.authenticationProvider.toString()))) {
-            servletContext.setAttribute("guestLoginEnabled", true)
-        } else{
-            servletContext.setAttribute("guestLoginEnabled", false)
-        }
-
-        //Import theme templates
+    def init = {
+        configPropertiesService.setTransactionTimeOUt()
+        configPropertiesService.setLoginEndPointUrl()
+        configPropertiesService.setLogOutEndPointUrl()
+        configPropertiesService.setGuestLoginEnabled()
         themeService.importTemplates(false)
-	}
+    }
 }
