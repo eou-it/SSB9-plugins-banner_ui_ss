4b54d0e812c6a2a96a382a9f7bc54581cbdc4bff
Harshal Bramhankar <Harshal.Bramhankar@ellucian.com>
2017-07-11 13:27:21 +0530

    Updation for Negative Sign After the Currency Symbol for Spanish Chile.

--- web-app/js/angular/load-angular-locale.js
+++ web-app/js/angular/load-angular-locale.js
@@ -1677,7 +1677,7 @@ switch(locale)  {
                 "maxFrac": 2,
                 "minFrac": 2,
                 "minInt": 1,
-                "negPre": "-\u00a4",
+                "negPre": "\u00a4-",
                 "negSuf": "",
                 "posPre": "\u00a4",
                 "posSuf": ""
