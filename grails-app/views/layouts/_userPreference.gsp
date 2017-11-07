c17294085db08e4d10250ca4ac3edab0bee9286e
Eshaan_kumar <eshaan.kumar@ellucian.com>
2017-11-07 15:07:13 +0530

    adding the http calls for backend

--- grails-app/views/layouts/_userPreference.gsp
+++ grails-app/views/layouts/_userPreference.gsp
@@ -5,10 +5,6 @@ Copyright 2017 Ellucian Company L.P. and its affiliates.
     <xe-popup-modal show="modalShown" title="{{title}}">
         <popup-content>
             <div>
-                <label style="margin-right: 1em" tabindex="0"
-                       for="preference" aria-label="'popup.preference.locale.heading' | xei18n"
-                        ng-bind="'popup.preference.locale.heading' | xei18n"/>
-                </label>
                 <xe-ui-select id="preference" ng-model="locale.selected" theme="select2">
                     <xe-ui-select-match>{{$select.selected.name}}</xe-ui-select-match>
                     <xe-ui-select-choices repeat="locale in localeList | propsFilter: {name: $select.search}">
@@ -18,7 +14,7 @@ Copyright 2017 Ellucian Company L.P. and its affiliates.
             </div>
         </popup-content>
         <popup-buttons>
-            <xe-button ng-click="testing()" id="button_0" xe-type="primary" xe-label="SAVE"  xe-disabled="false"></xe-button>
+            <xe-button ng-click="saveLocale()" id="button_0" xe-type="primary" xe-label="SAVE"  xe-disabled="false"></xe-button>
         </popup-buttons>
     </xe-popup-modal>
 </div>
--- web-app/js/preference/userpreference.js
+++ web-app/js/preference/userpreference.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
     angular.module('userPreference', ['xe-ui-components'])
-        .controller('PopupCtrl',[ '$scope','$timeout','$http', function($scope, $timeout, $http){
+        .controller('PopupCtrl',[ '$scope','$timeout','$http', function($scope, $timeout, $http ){
             $scope.modalShown = false;
             $scope.togglepopup = function() {
                 $scope.modalShown = !$scope.modalShown;
@@ -10,27 +10,59 @@
                 });
             };
 
-            $scope.title ="Preference Language ";
+            $scope.title =$.i18n.prop('userpreference.popup.language.heading');
+
             $scope.selectedLang ={};
+            $scope.localeList=[];
 
-           /* $scope.get('configUserPreference/fetch',function(){
+            $http.post("userPreference/locales").then(function (response) {
+               /* var localeData = response.data["locales"];
+                $(localeData).each(function(ind,ele){
+                    console.log(ele)
+                });
+                if(undefined!=response.data["selectedLocale"]){
+                    $scope.selectedLang = response.data["selectedLocale"];
+                    $scope.locale.selected = $scope.localeList[1];
+                }
 
-            });*/
-            $scope.testing = function(){
+                $scope.localeList = localeData;*/
+                console.log("LOCALE GOT");
+
+            },function (data, status, headers, config) {
+                alert("error");
+            });
+
+
+            $scope.saveLocale = function(){
                 var optionSelected = angular.element("#preference").find('span.select2-chosen').text();
-                $scope.selectedLang.name=optionSelected;
+                $scope.selectedLang.description=optionSelected;
                 for(var i=0;i<$scope.localeList.length;i++){
-                    if($scope.localeList[i].name ===optionSelected){
-                        $scope.selectedLang.id =$scope.localeList[i].id;
+                    if($scope.localeList[i].description ===optionSelected){
+                        $scope.selectedLang.locale =$scope.localeList[i].locale;
                     }
                 }
                 console.log($scope.selectedLang);
-
-                $http.post("userPreference/saveLocale", $scope.selectedLang).then(function (data, status, headers, config) {
-                    alert("success");
+                /* $http.post("userPreference/saveLocale", $scope.selectedLang).then(function (response, status) {
+                    $scope.togglepopup();
+                    var successmessage = $.i18n.prop('userpreference.notification.success.message');
+                    var successNotification = new Notification({
+                    message: successmessage,
+                    type: "success",
+                    component: successNotification,
+                    elementToFocus: successNotification
+                    });
+                    notifications.addNotification(successNotification);
                 },function (data, status, headers, config) {
-                    alert("error");
-                });
+                    $scope.togglepopup();
+                    var errorMessage = $.i18n.prop('userpreference.notification.failure.message');
+                    var errorNotification = new Notification({
+                    message: errorMessage,
+                    type: "error",
+                    component: errorNotification,
+                    elementToFocus: errorNotification
+                    });
+                    notifications.addNotification(errorNotification);
+                });*/
 
 
             };
