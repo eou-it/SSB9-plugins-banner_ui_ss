09dab7a1f4fdb2ba7aac75446544eefa5d1ae29c
Eshaan_kumar <eshaan.kumar@ellucian.com>
2017-11-08 15:51:26 +0530

    consuming the modal popup in sandbox app part of HRU-7471

--- grails-app/views/layouts/_userPreference.gsp
+++ grails-app/views/layouts/_userPreference.gsp
@@ -2,19 +2,19 @@
 Copyright 2017 Ellucian Company L.P. and its affiliates.
 --%>
 <div id="userPreferenceDiv" ng-app="userPreference" data-ng-controller="PopupCtrl">
-    <xe-popup-modal show="modalShown" title="{{title}}">
+    <xe-popup-modal show="modalShown">
         <popup-content>
             <div>
-                <xe-ui-select id="preference" ng-model="locale.selected" theme="select2">
-                    <xe-ui-select-match>{{$select.selected.name}}</xe-ui-select-match>
-                    <xe-ui-select-choices repeat="locale in localeList | propsFilter: {name: $select.search}">
-                        <div ng-bind-html="locale.name | highlight: $select.search"></div>
+                <xe-ui-select id="preference" ng-model="language.selected" theme="select2" ng-required="true">
+                    <xe-ui-select-match placeholder="${g.message(code: 'userpreference.popup.language.selectinput.placeholder')}">{{$select.selected.description}}</xe-ui-select-match>
+                    <xe-ui-select-choices repeat="localeLang in localeList | propsFilter: {description: $select.search}">
+                        <div ng-bind-html="localeLang.description | highlight: $select.search"></div>
                     </xe-ui-select-choices>
                 </xe-ui-select>
             </div>
         </popup-content>
         <popup-buttons>
-            <xe-button ng-click="saveLocale()" id="button_0" xe-type="primary" xe-label="SAVE"  xe-disabled="false"></xe-button>
+            <xe-button ng-click="saveLocale()" ng-Keydown="saveLocale()" id="saveLanguage" xe-disabled="disableButton" xe-type="primary" xe-label="SAVE" ></xe-button>
         </popup-buttons>
     </xe-popup-modal>
 </div>
--- web-app/js/preference/userpreference.js
+++ web-app/js/preference/userpreference.js
@@ -9,91 +9,66 @@
                     angular.element('#xeModalMask').attr('tabindex', 0).focus();
                 });
             };
-
             $scope.title =$.i18n.prop('userpreference.popup.language.heading');
 
-            $scope.selectedLang ={};
             $scope.localeList=[];
+            $scope.disableButton = true;
 
-            $http.post("userPreference/locales").then(function (response) {
-               /* var localeData = response.data["locales"];
-                $(localeData).each(function(ind,ele){
-                    console.log(ele)
-                });
+            /*if($('meta[name=menuBaseURL]').attr("content")){*/
+                var backendlocale = $('meta[name=menuBaseURL]').attr("content");
+            /*}*/
+            // To Fetch all Locales from DB
+            $http.get(backendlocale+"/userPreference/locales").then(function (response) {
+               var localesFromDB = response.data["locales"];
                 if(undefined!=response.data["selectedLocale"]){
-                    $scope.selectedLang = response.data["selectedLocale"];
-                    $scope.locale.selected = $scope.localeList[1];
+                    $scope.language.selected = response.data["selectedLocale"];
+                    $scope.disableButton = true;
                 }
-
-                $scope.localeList = localeData;*/
-                console.log("LOCALE GOT");
+                $scope.localeList = localesFromDB;
 
             },function (data, status, headers, config) {
+
                 alert("error");
             });
 
 
-            $scope.saveLocale = function(){
-                var optionSelected = angular.element("#preference").find('span.select2-chosen').text();
-                $scope.selectedLang.description=optionSelected;
-                for(var i=0;i<$scope.localeList.length;i++){
-                    if($scope.localeList[i].description ===optionSelected){
-                        $scope.selectedLang.locale =$scope.localeList[i].locale;
-                    }
+            $scope.$watch("language.selected", function(newVal, oldVal) {
+                if (undefined != $scope.language.selected && null!=$scope.language.selected){
+                    $scope.disableButton = false;
                 }
-                console.log($scope.selectedLang);
-                /* $http.post("userPreference/saveLocale", $scope.selectedLang).then(function (response, status) {
+            });
+
+
+            // To pass the selected Locale to backend and set in DB
+            $scope.saveLocale = function(){
+                console.log("HEREE");
+                $http({
+                    method: 'POST',
+                    url: "userPreference/saveLocale",
+                    data: $scope.language.selected
+                }).then(function (response, status) {
                     $scope.togglepopup();
                     var successmessage = $.i18n.prop('userpreference.notification.success.message');
                     var successNotification = new Notification({
-                    message: successmessage,
-                    type: "success",
-                    component: successNotification,
-                    elementToFocus: successNotification
+                        message: successmessage,
+                        type: "success",
+                        component: successNotification,
+                        elementToFocus: successNotification
                     });
                     notifications.addNotification(successNotification);
                 },function (data, status, headers, config) {
                     $scope.togglepopup();
                     var errorMessage = $.i18n.prop('userpreference.notification.failure.message');
                     var errorNotification = new Notification({
-                    message: errorMessage,
-                    type: "error",
-                    component: errorNotification,
-                    elementToFocus: errorNotification
+                        message: errorMessage,
+                        type: "error",
+                        component: errorNotification,
+                        elementToFocus: errorNotification
                     });
                     notifications.addNotification(errorNotification);
-                });*/
-
-
+                });
             };
-
-            $scope.localeList = [
-                {id:"ar",name:"Arabic"},
-                {id:"en_AU",name:"English Australia"},
-                {id:"en_GB",name:"English United Kingdom"},
-                {id:"en_IE",name:"English Ireland"},
-                {id:"en_IN",name:"English India"},
-                {id:"es",name:"Spanish"},
-                {id:"fr",name:"French"},
-                {id:"fr_CA",name:"French Canada"},
-                {id:"pt",name:"Portuguese"},
-                {id:"es_MX",name:"Spanish (Mexico)"},
-                {id:"es_PE",name:"Spanish (Peru)"},
-                {id:"es_CO",name:"Spanish (Colombia)"},
-                {id:"es_DO",name:"Spanish (Dominican Republic)"},
-                {id:"es_PR",name:"Spanish (Puerto Rico)"},
-                {id:"es_VE",name:"Spanish (Venezuela)"},
-                {id:"es_CL",name:"Spanish (Canary Islands)"},
-                {id:"es_EC",name:"Spanish (Ecuador)"},
-                {id:"es_CR",name:"Spanish (Costa Rica)"},
-                {id:"es_PA",name:"Spanish (Panama)"},
-                {id:"es_GT",name:"Spanish (Guatemala)"},
-                {id:"es_AR",name:"Spanish (Argentina)"},
-                {id:"ar_SA",name:"Arabic (Saudi Arabia)"}
-            ];
-
-            $scope.locale = {};
-            /*$scope.locale.selected = $scope.localeList[1];*/
+            $scope.language = {};
 
         }]);
 })();
