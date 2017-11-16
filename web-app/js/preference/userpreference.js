4858e80014a336041ae1af84af7b8606479d1201
Eshaan_kumar <eshaan.kumar@ellucian.com>
2017-11-16 10:52:30 +0530

    fixing the notification and default focus issue for user setting language part of HRU-7472

--- web-app/js/preference/userpreference.js
+++ web-app/js/preference/userpreference.js
@@ -7,32 +7,31 @@
     angular.module('userPreference', ['xe-ui-components'])
         .controller('PopupCtrl',[ '$scope','$timeout','$http', function($scope, $timeout, $http ){
             $scope.modalShown = false;
+            $scope.disableButton = true;
             if($('meta[name=menuBaseURL]').attr("content")){
                 var backendlocale = $('meta[name=menuBaseURL]').attr("content");
             }
-            notifications.clearNotifications();
+            $scope.prevSelected={};
             $scope.togglepopup = function() {
+                notifications.clearNotifications();
                 $scope.modalShown = !$scope.modalShown;
+                delete $scope.language.selected;
                 $timeout(function () {
                     angular.element('#xeModalMask').attr('tabindex', 0).focus();
+                    angular.element('#preference').find('input')[1].focus();
                     // To Fetch all Locales from DB
                     $http.get(backendlocale+"/userPreference/locales").then(function (response) {
                         var localesFromDB = response.data["locales"];
                         if(undefined!=response.data["selectedLocale"]){
                             $scope.language.selected = response.data["selectedLocale"];
-                            $scope.disableButton = true;
+                            $scope.prevSelected = response.data["selectedLocale"];
                         }
+                        $scope.disableButton = true;
                         $scope.localeList = localesFromDB;
 
                     },function (data, status, headers, config) {
-                        var errorMessage = $.i18n.prop('userpreference.notification.failure.message');
-                        var errorNotification = new Notification({
-                            message: errorMessage,
-                            type: "error",
-                            component: errorNotification,
-                            elementToFocus: errorNotification
-                        });
-                        notifications.addNotification(errorNotification);
+                        $scope.closePopup();
+                        errorNotification();
                     });
                 });
             };
@@ -43,17 +42,19 @@
 
             $scope.popupTitle = $.i18n.prop('userpreference.popup.language.heading');
             $scope.localeList=[];
-            $scope.disableButton = true;
 
-            $scope.$watch("language.selected", function(newVal, oldVal) {
-                if (undefined != $scope.language.selected && null!=$scope.language.selected){
+            $scope.$watch("language.selected", function (newVal, oldVal) {
+                if (undefined != $scope.prevSelected && undefined != $scope.language.selected && $scope.language.selected.locale === $scope.prevSelected.locale) {
+                    $scope.disableButton = true;
+                } else if (newVal != oldVal) {
                     $scope.disableButton = false;
                 }
             });
 
             // To pass the selected Locale to backend and set in DB
             $scope.saveLocale = function(){
-                $scope.disableButton = true;
+                /*setTimeout(function() { angular.element("#saveLanguage").focusout(); }, 10);*/
+                /*$scope.disableButton = true;*/
                 $http({
                     method: 'POST',
                     url: backendlocale+"/userPreference/saveLocale",
@@ -61,42 +62,41 @@
                     cache: false
                 }).then(function (response, status) {
                     $scope.closePopup();
-                    notifications.clearNotifications();
-                    var successmessage = $.i18n.prop('userpreference.notification.success.message');
-                    var successNotification = new Notification({
-                        message: successmessage,
-                        type: "success",
-                        component: successNotification,
-                        elementToFocus: successNotification
-                    });
-                    notifications.addNotification(successNotification);
+                    errorNotification();
                 },function (data, status, headers, config) {
                     $scope.closePopup();
-                    notifications.clearNotifications();
-                    var errorMessage = $.i18n.prop('userpreference.notification.failure.message');
-                    var errorNotification = new Notification({
-                        message: errorMessage,
-                        type: "error",
-                        component: errorNotification,
-                        elementToFocus: errorNotification
-                    });
-                    notifications.addNotification(errorNotification);
+                    errorNotification();
                 });
             };
 
-            $scope.errorMessage = function(){
-                $scope.closePopup();
+            $scope.language = {};
+
+            function successNotification(){
                 notifications.clearNotifications();
+                var element1 = angular.element("#notification-center").find('a:first');
+                var successmessage = $.i18n.prop('userpreference.notification.success.message');
+                var successNotification = new Notification({
+                    message: successmessage,
+                    type: "success",
+                    component: successNotification,
+                    elementToFocus: element1
+                });
+                notifications.addNotification(successNotification);
+            }
+
+            function errorNotification(){
+                notifications.clearNotifications();
+                /*var element1 = angular.element("#notificationCenterAriaInfo").find('ul.error-container:first');*/
+                var element1 = angular.element("#notification-center").find('a:first');
                 var errorMessage = $.i18n.prop('userpreference.notification.failure.message');
                 var errorNotification = new Notification({
                     message: errorMessage,
                     type: "error",
                     component: errorNotification,
-                    elementToFocus: errorNotification
+                    elementToFocus: element1
                 });
                 notifications.addNotification(errorNotification);
-            };
-            $scope.language = {};
+            }
 
         }]);
 })();
