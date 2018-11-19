cede9f38476a060e090655a052f36e45b69ac54a
Vinay H S <vinay.hs@ellucian.com>
2018-11-19 12:28:22 +0530

    EXTZ-2886 implemented code review comment.

--- grails-app/taglib/net/hedtech/banner/ui/ThemeTagLib.groovy
+++ grails-app/taglib/net/hedtech/banner/ui/ThemeTagLib.groovy
@@ -18,7 +18,7 @@ class ThemeTagLib {
             }
             mepCodeParam=''
         }
-        String themeTemplate = themeConfig.template ? themeConfig.template : null
+        String themeTemplate = themeConfig.template ?: null
         String cssLink
         if(themeName && themeTemplate) {
             cssLink = "<link rel='stylesheet' type='text/css' "
