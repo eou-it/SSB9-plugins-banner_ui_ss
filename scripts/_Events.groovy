798575b22eae9135a27516d8ee4de4941f11f836
Eshaan_kumar <eshaan.kumar@ellucian.com>
2018-11-26 10:00:22 +0530

    merging 9.28.5 to banner_ui_ss.git

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,5 +1,6 @@
+
 /*******************************************************************************
- Copyright 2009-2016 Ellucian Company L.P. and its affiliates.
+ Copyright 2009-2018 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 
 eventCompileEnd = {
@@ -10,10 +11,13 @@ eventCompileEnd = {
     Class ThemeScssGenerator = classLoader.loadClass("net.hedtech.banner.common.ThemeScssGenerator", true)
     def themeScssGenerator = ThemeScssGenerator.newInstance()
     def scssFilePath = "${basedir}/web-app/css/theme/"
-    def scssFileName = grails.util.Metadata.current.'app.name'+".scss"
+    def appName = grails.util.Metadata.current.'app.name'
+    def appVersion = grails.util.Metadata.current.'app.version'
+    def versionText = appVersion ? ("-" + appVersion).replaceAll(/\./, '_') : ""
+    def scssFileName = (appName + versionText) + ".scss"
     def scssFile = scssFilePath+scssFileName
     try {
-        themeScssGenerator.generateThemeSCSSFile(scssFile);
+        themeScssGenerator.generateThemeSCSSFile(scssFile, appName, appVersion);
     } catch (FileNotFoundException e) {
         println "Unable to generate theme SCSS file - unexpected exception"
         e.printStackTrace()
