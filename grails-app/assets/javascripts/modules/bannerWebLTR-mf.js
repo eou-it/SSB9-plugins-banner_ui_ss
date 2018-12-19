63eba39206106c55a90c5c099b3b9c540b27b146
Avinash Hinduja <avinash.hinduja@ellucian.com>
2018-12-19 10:07:43 +0530

    updating extensibilityAngular instead of extensibilityJQuery into banenrwebLTR

--- grails-app/assets/javascripts/modules/bannerWebLTR-mf.js
+++ grails-app/assets/javascripts/modules/bannerWebLTR-mf.js
@@ -1,7 +1,7 @@
 //= require modules/angularApp-mf.js
 //= require modules/extensibilityCommon-mf.js
 //= require modules/bannerSelfServiceCommonLTR-mf.js
-//= require modules/extensibilityJQuery-mf.js
+//= require modules/extensibilityAngular-mf.js
 //= require about/about.js
 //= require menu/tools-menu.js
 //= require preference/userpreference.js
