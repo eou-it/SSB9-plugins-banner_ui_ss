50018fba150d1bef45ad38326600dd798f640217
gurunath_dk <gurunat.dk@ellucian.com>
2019-01-28 10:49:17 +0530

    NOJIRA: Updating the copyright

--- src/main/groovy/net/hedtech/banner/common/ThemeScssGenerator.groovy
+++ src/main/groovy/net/hedtech/banner/common/ThemeScssGenerator.groovy
@@ -1,5 +1,5 @@
 /** *****************************************************************************
- Copyright 2009-2018 Ellucian Company L.P. and its affiliates.
+ Copyright 2009-2019 Ellucian Company L.P. and its affiliates.
  ****************************************************************************** */
 package net.hedtech.banner.common
 
