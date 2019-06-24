8279ef6413ba5d7c82f32387b3cd9b54d271b2ee
Vinay H S <vinay.hs@ellucian.com>
2019-06-24 15:36:04 +0530

    EXTZ-3483 Theme Editor: Grails 3 maxFileUploadSize is 128KB by Default.

--- grails-app/conf/runtime.groovy
+++
@@ -1,7 +0,0 @@
-/*******************************************************************************
- Copyright 2019 Ellucian Company L.P. and its affiliates.
- *******************************************************************************/
-
-//set maximum upload file size in bytes for uploading themes/templates inside themeEditor
-grails.controllers.upload.maxFileSize = 1000000 //1MB
-grails.controllers.upload.maxRequestSize = 1000000 //1MB
