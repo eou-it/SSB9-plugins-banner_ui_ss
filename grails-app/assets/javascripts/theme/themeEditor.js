3aacf7dc8a4570b11393ca6960ebc7f3073df84a
chirayuh <Chirayu.H@ellucian.com>
2020-12-11 17:57:58 +0530

    HRU-9866: Update copyright year.

--- grails-app/assets/javascripts/theme/themeEditor.js
+++ grails-app/assets/javascripts/theme/themeEditor.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright 2016-2019 Ellucian Company L.P. and its affiliates.
+ Copyright 2016-2020 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 /* global notifications */
 (function() {
