90be7e78f68318f370a1a58926647068b1fb663a
chirayuh <Chirayu.H@ellucian.com>
2021-01-07 15:56:49 +0530

    HRU-9878:Set focus on Ellucian policy link.

--- grails-app/assets/javascripts/about/about.js
+++ grails-app/assets/javascripts/about/about.js
@@ -19,7 +19,7 @@
             $scope.toggleModal = function() {
                 $scope.modalShown = !$scope.modalShown;
                 $timeout(function () {
-                    angular.element('#xeModalContainer').attr('tabindex', 0).focus();
+                    angular.element('#xeModalPrivacyLink').focus();
                 });
             };
             $scope.aboutApi = {
